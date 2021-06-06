aa07d8c43f525706a2f79133b51ca5e6feec75f1
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-06 14:25:04 -0700

    setting up react

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,7 +18,7 @@ function Nav() {
 
     function categorySelected(name) {
         console.log(`${name} clicked`)
-      }
+    }
 
     return
     <header>
@@ -42,7 +42,7 @@ function Nav() {
                         className="mx-1"
                         key={category.name}
                     >
-                        <span onClick={categorySelected} >
+                        <span onClick={() => categorySelected(category.name)} >
                             {category.name}
                         </span>
                     </li>
