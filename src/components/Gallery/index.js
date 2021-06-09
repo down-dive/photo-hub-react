922abb480f31a38f2e5000c1b5da4d12f91f4a51
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-08 22:08:19 -0700

    photolist component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import PhotoList from '../PhotoList';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+// __tests__/Gallery.test.js
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Gallery from '..'
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
 
-function Gallery({ currentCategory }) {
-  const { name, description } = currentCategory;
-  return (
-    <section>
-      <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
-      <p>{description}</p>
-      <PhotoList  />
-    </section>
-  );
-}
+afterEach(cleanup)
 
-export default Gallery;
+describe('Gallery component', () => {
+
+  it('renders', () => {
+    render(<Gallery currentCategory={portrait} />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Gallery currentCategory={portrait} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+});
+
+it('renders', () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+})
