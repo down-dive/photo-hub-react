e34642a56c75ca4230bec64420b5783cd494db53
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-09 17:32:50 -0700

    contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,7 +12,7 @@ function ContactForm() {
           <form id="contact-form">
             <div>
               <label htmlFor="name">Name:</label>
-              <input type="text" defaultValue={name} name="name" />            </div>
+              <input type="text" defaultValue={name} onChange={handleChange} name="name" />            </div>
             <div>
               <label htmlFor="email">Email address:</label>
               <input type="email" defaultValue={email} name="email" />
