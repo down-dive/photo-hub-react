f458689d2699e8a55bcb7167fb6591490ebcb34b
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-09 18:00:21 -0700

    contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,8 @@ import { validateEmail } from '../../utils/helpers';
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
 
     if (e.target.name === 'email') {
         const isValid = validateEmail(e.target.value);
