4f9511dd9bbb59f30b21802db0f582d43bd4d1b3
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-10 09:15:09 -0700

    navigation and contact compomemts are done

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -4,3 +4,15 @@ import '@testing-library/jest-dom/extend-expect';
 import Contact from '..';
 
 afterEach(cleanup);
+
+describe('Contact component', () => {
+    it('renders', () => {
+      render(<Contact />);
+    });
+  
+    it('matches snapshot', () => {
+      const { asFragment } = render(<Contact />);
+      
+      expect(asFragment()).toMatchSnapshot();
+    });
+  })
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
-    currentCategory,
     contactSelected,
-    setContactSelected
+    currentCategory,
+    setContactSelected,
   } = props;
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -33,14 +29,16 @@ function Nav(props) {
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${currentCategory.name === category.name && !contactSelected && `navActive`
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
             >
-              <span onClick={() => {
-                setCurrentCategory(category);
-                setContactSelected(false);
-              }}
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
               >
                 {capitalizeFirstLetter(category.name)}
               </span>
