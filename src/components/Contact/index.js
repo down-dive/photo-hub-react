e2820f59b04b2e8b6bd7f2bf8db4468a4cce400f
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-09 17:51:37 -0700

    contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,31 +5,33 @@ function ContactForm() {
     const { name, email, message } = formState;
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        setFormState({ ...formState, [e.target.name]: e.target.value })
     }
-      
-      console.log(formState);
     // JSX
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
     return (
         <section>
-          <h1>Contact me</h1>
-          <form id="contact-form">
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input type="text" defaultValue={name} onChange={handleChange} name="name" />            </div>
-            <div>
-              <label htmlFor="email">Email address:</label>
-              <input type="email" defaultValue={email} onChange={handleChange} name="email" />
-            </div>
-            <div>
-              <label htmlFor="message">Message:</label>
-              <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
-            </div>
-            <button type="submit">Submit</button>
-          </form>
+            <h1>Contact me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />            </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </section>
-        );
-    }
+    );
+}
 
-    export default ContactForm;
+export default ContactForm;
